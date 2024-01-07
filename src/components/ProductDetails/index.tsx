a8b622e874cdf7c13f31818f84122e5def9b09f5
Vitor Hugo <vitorhugo02cc@gmail.com>
2024-01-07 19:38:31 -0300

    feature: melhorando a estilizacao e o desing do componente ProductDetails

--- src/components/ProductDetails/index.tsx
+++ src/components/ProductDetails/index.tsx
@@ -6,6 +6,7 @@ import { Product } from '../../data';
 import { useDetailsModal } from '../../hooks/detailsModal/useDetailsModal';
 import AddToCartBtn from '../AddToCartBtn';
 import Rating from '../Rating';
+import formatCurrency from '../../utils/formatCurrency';
 
 interface ProductDetailsProps {
     product: Product;
@@ -22,22 +23,44 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
                         <span className="productDetails__closeButton" onClick={changeDetailModalVisibility}>
                             &times;
                         </span>
-                        <h2>{product.name}</h2>
-                        <p>{product.description}</p>
+                        <div className='productDetails__header'>
+                            <h2>{product.name}</h2>
+                            <Rating
+                                value={product.rating}
+                                color='#FFA41C'
+                                text={`(${product.numReviews})`}
+                            />
+                        </div>
+
+                        <img className="productDetails__img" src={product.image} alt="" />
+
+                        <div className="productDetails__paymentInfo">
+                            <h2>{formatCurrency(product.price)}</h2>
+                            <AddToCartBtn product={product} />
+                        </div>
+
+                        <div className='productDetails__description'>
+                            <span>SOBRE</span>
+                            <p>{product.description}</p>
+                        </div>
 
                         <ul>
+                            <span className='productDetails__reviewHeader'>VEJA AS AVALIAÇÕES DO PRODUTO</span>
                             {product.reviews.map((comment, index) => (
-                                <div className='productDetails__reviewWrapper'>
-                                    <h5 key={index}>{comment.author}</h5>
+                                <div className='productDetails__reviewContainer'>
+                                    <hr />
+                                    <div className='productDetails__reviewWrapper'>
+                                        <h5 key={index}>{comment.author}</h5>
+                                        <Rating
+                                            value={comment.rating}
+                                            color={"#FFA41C"}
+                                        />
+                                    </div>
+
                                     <li key={index}>{comment.text}</li>
-                                    <Rating 
-                                        value={product.rating}
-                                        color={"#FFA41C"}
-                                    />
                                 </div>
                             ))}
                         </ul>
-                        <AddToCartBtn product={product} />
                     </div>
                 </div>
             )}
